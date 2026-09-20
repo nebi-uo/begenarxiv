05e2ca2db1120c74c3b33f619250a7a84b038eda
Nebiuo <nebiyoutahoa@gmail.com>
2026-09-20 20:46:29 +0300

    Router update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 import CataloguePage from './pages/CataloguePage';
 import MezmurPage from './pages/MezmurPage';
+// import TunerPage from './pages/TunerPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CataloguePage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/catalogue" element={<CataloguePage />} />
         <Route path="/mezmur/:id" element={<MezmurPage />} />
       </Routes>
     </BrowserRouter>
