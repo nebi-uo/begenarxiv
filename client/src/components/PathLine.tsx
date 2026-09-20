c5ad34d9bed74e1a4cb1bc7f0e5eab85e8b3696f
Nebiuo <nebiyoutahoa@gmail.com>
2026-09-20 11:03:48 +0300

    Path line component initialization

---
+++ client/src/components/PathLine.tsx
@@ -0,0 +1,11 @@
+export default function PathLine() {
+  return (
+    <div
+      className="absolute left-8 top-0 bottom-0 w-0.5 -z-10"
+      style={{
+        backgroundImage:
+          'repeating-linear-gradient(to bottom, #D8D5CF 0, #D8D5CF 6px, transparent 6px, transparent 14px)',
+      }}
+    />
+  );
+}
